b4070395d66d9b106ff993d0d524a5db64d1732f
Shivakalyan Gupta <polashivakalyan@gmail.com>
2025-06-30 23:14:52 +0530

    Configured GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Shivakalyan_Portfolio/', // ✅ This line is critical for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
